923b72d008ff7a5f3e4050331ab1accc7145a8fa
Caspar <caspar@everspring.app>
2023-01-16 22:41:26 +0100

    grid

--- defs.py
+++ defs.py
@@ -3,3 +3,11 @@ screen_width = 400
 background_color = (91, 91, 255)
 text_color = (0, 0, 0)
 game_font = 'fonts/Big Pixel demo.otf'
+
+number_of_mines = {
+    'easy': 10,
+    'medium': 40,
+    'hard': 99,
+    'expert': 150,
+    'insane': 400
+}
--- utils/Grid.py
+++ utils/Grid.py
@@ -1,117 +1,91 @@
 import dataclasses
 import random
+from typing import List
 
 import numpy as np
+
+from defs import number_of_mines
+from utils.Index import Index
+
+
+@dataclasses.dataclass
+class Tile:
+    type: str
+    center: Index
+    orientation: int
 
 
 @dataclasses.dataclass
 class Grid:
-    def __init__(self):
-        self.arr = np.zeros((22, 10), int)
-        self.lost = False
+    def __init__(self, level):
+        match level:
+            case 'easy':
+                self.mine_map = np.zeros((9, 9), int)
+                self.difficulty = level
+                self.display_map = np.zeros((9, 9), int)
+            case 'medium':
+                self.mine_map = np.zeros((16, 16), int)
+                self.difficulty = level
+                self.display_map = np.zeros((16, 16), int)
+            case 'hard':
+                self.mine_map = np.zeros((16, 30), int)
+                self.difficulty = level
+                self.display_map = np.zeros((16, 30), int)
+            case 'expert':
+                self.mine_map = np.zeros((16, 30), int)
+                self.difficulty = level
+                self.display_map = np.zeros((16, 30), int)
+            case 'insane':
+                self.mine_map = np.zeros((20, 32), int)
+                self.difficulty = level
+                self.display_map = np.zeros((20, 32), int)
 
-    def spawn(self):
-        """
-        spawns a random piece at the top
-        :return:
-        """
-        piece = random.randint(0, 6)
-        updated_arr = self.arr.copy()
-        match piece:
-            # square piece
-            case 0:
-                if (updated_arr[0][4] == 1) or (updated_arr[0][5] == 1) \
-                        or (updated_arr[1][4] == 1) or (updated_arr[1][5] == 1):
-                    self.lost = True
-                else:
-                    updated_arr[0][4] = 2
-                    updated_arr[0][5] = 2
-                    updated_arr[1][4] = 2
-                    updated_arr[1][5] = 2
-            # line piece
-            case 1:
-                if (updated_arr[0][4] == 1) or (updated_arr[0][5] == 1) \
-                        or (updated_arr[0][3] == 1) or (updated_arr[0][6] == 1):
-                    self.lost = True
-                else:
-                    updated_arr[0][3] = 2
-                    updated_arr[0][5] = 2
-                    updated_arr[0][4] = 2
-                    updated_arr[0][6] = 2
-            # S piece
-            case 2:
-                if (updated_arr[0][5] == 1) or (updated_arr[0][6] == 1) \
-                        or (updated_arr[1][4] == 1) or (updated_arr[1][5] == 1):
-                    self.lost = True
-                else:
-                    updated_arr[0][5] = 2
-                    updated_arr[0][6] = 2
-                    updated_arr[1][4] = 2
-                    updated_arr[1][5] = 2
-            # reverse s piece
-            case 3:
-                if (updated_arr[0][4] == 1) or (updated_arr[0][5] == 1) \
-                        or (updated_arr[1][5] == 1) or (updated_arr[1][6] == 1):
-                    self.lost = True
-                else:
-                    updated_arr[0][4] = 2
-                    updated_arr[0][5] = 2
-                    updated_arr[1][5] = 2
-                    updated_arr[1][6] = 2
-            # T piece
-            case 4:
-                if (updated_arr[0][4] == 1) or (updated_arr[0][5] == 1) \
-                        or (updated_arr[0][6] == 1) or (updated_arr[1][5] == 1):
-                    self.lost = True
-                else:
-                    updated_arr[0][4] = 2
-                    updated_arr[0][5] = 2
-                    updated_arr[0][6] = 2
-                    updated_arr[1][5] = 2
-            # L piece
-            case 5:
-                if (updated_arr[0][6] == 1) or (updated_arr[1][4] == 1) \
-                        or (updated_arr[1][5] == 1) or (updated_arr[1][6] == 1):
-                    self.lost = True
-                else:
-                    updated_arr[0][6] = 2
-                    updated_arr[1][4] = 2
-                    updated_arr[1][5] = 2
-                    updated_arr[1][6] = 2
-            # J piece
-            case 6:
-                if (updated_arr[0][4] == 1) or (updated_arr[1][4] == 1) \
-                        or (updated_arr[1][5] == 1) or (updated_arr[1][6] == 1):
-                    self.lost = True
-                else:
-                    updated_arr[0][4] = 2
-                    updated_arr[1][4] = 2
-                    updated_arr[1][5] = 2
-                    updated_arr[1][6] = 2
-        self.arr = updated_arr
+        self.initialize_mines()
+        self.initialize_board()
 
-    def update(self):
-        """
-        Handles the movement of the piece down, will move down the piece by 1 tile on the array
-        :return:
-        """
-        updated_arr = self.arr.copy()
-        indices = np.unique(np.where(updated_arr == 2)[0])
-        for index_y in reversed(indices):
-            for index_x, element in enumerate(updated_arr[index_y]):
-                if element == 2:
-                    if (index_y >= 20) or (updated_arr[index_y + 1][index_x] == 1):
-                        self.arr = np.where(self.arr == 2, 1, self.arr)
-                        return
-                    else:
-                        updated_arr[index_y + 1][index_x] = 2
-                        updated_arr[index_y][index_x] = 0
-        self.arr = updated_arr
+    def initialize_mines(self):
+        # find the number of mines to initialize
+        mine_number = number_of_mines[self.difficulty]
+        mine_indexes = []
+        while len(mine_indexes) < mine_number:
+            num_x = random.randint(0, len(self.mine_map[0]) - 1)
+            num_y = random.randint(0, len(self.mine_map) - 1)
+            if (num_x, num_y) not in mine_indexes:
+                mine_indexes.append((num_x, num_y))
+        for i in mine_indexes:
+            self.mine_map[i[1]][i[0]] = 1
+
+    def initialize_board(self):
+        for y in range(len(self.mine_map)):
+            for x in range(len(self.mine_map[0])):
+                self.display_map[y][x] = self.count_mines(Index(x=x, y=y))
+
+    def count_mines(self, index: Index):
+        if index.y - 1 < 0:
+            y_range = (0, index.y + 2)
+        elif index.y + 1 > len(self.mine_map) - 1:
+            y_range = (index.y - 1, len(self.mine_map) - 1)
+        else:
+            y_range = (index.y - 1, index.y + 2)
+
+        if index.x - 1 < 0:
+            x_range = (0, index.x + 2)
+        elif index.x + 1 > len(self.mine_map[0]) - 1:
+            x_range = (index.x - 1, len(self.mine_map[0]) - 1)
+        else:
+            x_range = (index.x - 1, index.x + 2)
+
+        mine_count = 0
+        for y in range(y_range[0], y_range[1]):
+            for x in range(x_range[0], x_range[1]):
+                if (x == index.x) and (y == index.y) and (self.mine_map[y][x] == 1):
+                    mine_count = -1
+                    return mine_count
+                elif self.mine_map[y][x] == 1:
+                    mine_count += 1
+        return mine_count
 
 
 if __name__ == '__main__':
-    a = Grid()
-    a.spawn()
-    print(a.arr)
-    a.update()
-    print(a.arr)
+    a = Grid(level='hard')
+    print(a.display_map)
